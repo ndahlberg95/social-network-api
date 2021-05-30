24d88585739f78a82480c36b9c0e405bbcadae6a
Nora Dahlberg <ndahlberg95@gmail.com>
2021-05-30 09:37:38 -0600

    add CREATE, POST, and DESTROY routes

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,9 +1,73 @@
+const router = require('express').Router()
+const Thought = require('../../models/Thought');
+
 //GET to get all thoughts
+router.get('/', (req, res) => {
+    Thought.findAll()
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 //GET to get a single thought by its _id
 
 //POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router.post('/', (req, res) => {
+    // expects {thoughtText: 'Hahaha!', createdAt: 9:25, username: 'myusername'}
+    Thought.create({
+        thoughtText: req.body.thoughtText,
+        createdAt: req.body.createdAt,
+        username: req.body.username
+    })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    res.send('Thought created!')
+});
 
 //PUT to update a thought by its _id
+router.put('/:_id', (req, res) => {
+    // expects {_id: '1', thoughtText: "Hahaha!"}
+    Thought.update(req.thoughtText, {
+        where: {
+            _id: req.params._id
+        }
+    })
+        .then(dbThoughtData => {
+            if (!dbThoughtData[0]) {
+                res.status(404).json({ message: 'No thought found with this _id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 //DELETE to remove a thought by its _id
+router.delete('/:_id', (req, res) => {
+    Thought.destroy({
+        where: {
+            _id: req.params._id
+        }
+    })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -3,36 +3,70 @@ const User = require('../../models/User');
 
 //GET all users
 router.get('/', (req, res) => {
-    //     User.findAll()
-    //         .then(dbTagData => res.json(dbTagData))
-    //         .catch(err => {
-    //             console.log(err);
-    //             res.status(500).json(err);
-    //         });
-    User.find().then(
-        resp => res.json(resp)
-    )
+    User.findAll()
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 //GET  a single user by its _id and populated thought and friend data
 
 //POST a new user
 router.post('/', (req, res) => {
-    // expects {id: '1', tag-name: 'tag'}
-    // Tag.create({
-    //     id: req.body.id,
-    //     tag_name: req.body.tag_name
-    // })
-    //     .then(dbTagData => res.json(dbTagData))
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(500).json(err);
-    //     });
-    res.send('got the post route!')
+    // expects {username: 'myusername', email: 'user@email.com'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+    res.send('User created!')
 });
 
 //PUT to update a user by its _id
+router.put('/:_id', (req, res) => {
+    // expects {_id: '1', username: "myusername"}
+    User.update(req.username, {
+        where: {
+            _id: req.params._id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No User found with this _id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 //DELETE to remove user by its _id
+router.delete('/:_id', (req, res) => {
+    User.destroy({
+        where: {
+            _id: req.params._id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUsertData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
-module.exports = router
+module.exports = router;
