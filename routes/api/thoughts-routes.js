e654cab94885d488164a0b0260a355770f968560
Nora Dahlberg <ndahlberg95@gmail.com>
2021-07-13 13:43:14 -0400

    update README.md

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -3,7 +3,7 @@ const Thought = require('../../models/Thought');
 
 //GET to get all thoughts
 router.get('/', (req, res) => {
-    Thought.findAll()
+    Thought.find()
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => {
             console.log(err);
@@ -12,6 +12,24 @@ router.get('/', (req, res) => {
 });
 
 //GET to get a single thought by its _id
+// router.get('/:id', (req, res) => {
+//     Thought.findOne({
+//         where: {
+//             _id: req.params._id
+//         }
+//     })
+//         .then(dbThoughtData => {
+//             if (!dbThoughtData) {
+//                 res.status(404).json({ message: 'No thought found with this _id' });
+//                 return;
+//             }
+//             res.json(dbThoughtData);
+//         })
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
 
 //POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
 router.post('/', (req, res) => {
@@ -69,5 +87,11 @@ router.delete('/:_id', (req, res) => {
             res.status(500).json(err);
         });
 });
+
+// /api/thoughts/:thoughtId/reactions
+
+//POST to create a reaction stored in a single thought's reactions array field
+
+//DELETE to pull and remove a reaction by the reaction's reactionId value
 
 module.exports = router;
--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -1,72 +1,26 @@
-const router = require('express').Router()
-const User = require('../../models/User');
+const router = require('express').Router();
 
-//GET all users
-router.get('/', (req, res) => {
-    User.findAll()
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
 
-//GET  a single user by its _id and populated thought and friend data
+router.route('/')
+    .get(getAllUsers)
+    .post(createUser);
 
-//POST a new user
-router.post('/', (req, res) => {
-    // expects {username: 'myusername', email: 'user@email.com'}
-    User.create({
-        username: req.body.username,
-        email: req.body.email
-    })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
-    res.send('User created!')
-});
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
 
-//PUT to update a user by its _id
-router.put('/:_id', (req, res) => {
-    // expects {_id: '1', username: "myusername"}
-    User.update(req.username, {
-        where: {
-            _id: req.params._id
-        }
-    })
-        .then(dbUserData => {
-            if (!dbUserData[0]) {
-                res.status(404).json({ message: 'No User found with this _id' });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
-
-//DELETE to remove user by its _id
-router.delete('/:_id', (req, res) => {
-    User.destroy({
-        where: {
-            _id: req.params._id
-        }
-    })
-        .then(dbUserData => {
-            if (!dbUserData) {
-                res.status(404).json({ message: 'No user found with this id' });
-                return;
-            }
-            res.json(dbUsertData);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-});
+router.route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend)
 
 module.exports = router;
